864977f350281569dc86dd876e1fcc1f3313d146
Arka Sourav Karmakar <arka@Arkas-MacBook-Pro.local>
2022-08-20 00:36:16 +0530

    updates

---
+++ array/removing_element.js
@@ -0,0 +1,21 @@
+let numbers = [1, 2, 3, 4];
+
+//End
+const last = numbers.pop();
+console.log(last); //--> 4
+console.log(numbers); //-->  [1, 2, 3]
+
+//Beginning
+numbers = [1, 2, 3, 4];
+const first = numbers.shift();
+console.log(first); //--> 1
+console.log(numbers); //-->  [2, 3, 4]
+
+//Middle
+numbers = [1, 2, 3, 4];
+numbers.splice(1, 2);
+console.log(numbers); //-->  [1, 4]
+
+//to empty an array
+numbers.length = 0;
+console.log(numbers); //--> []
