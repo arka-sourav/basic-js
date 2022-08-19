2d217dda3c81cfba022e70e727e6d81216a80c8a
Arka Sourav Karmakar <arka@Arkas-MacBook-Pro.local>
2022-08-20 01:42:29 +0530

    updates

--- array/removing_element.js
+++ array/removing_element.js
@@ -13,8 +13,9 @@ console.log(numbers); //-->  [2, 3, 4]
 
 //Middle
 numbers = [1, 2, 3, 4];
-numbers.splice(1, 2);
+const removedItems = numbers.splice(1, 2);
 console.log(numbers); //-->  [1, 4]
+console.log("removedItems", removedItems); //--> [2, 3]
 
 //to empty an array
 numbers.length = 0;
